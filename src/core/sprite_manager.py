4b27a120ee3ef7fe57a74eec6ad72ed05b08999d
PetitCh <josepetitch@gmail.com>
2026-08-06 20:29:53 -0400

    Archivo (de momento vacío) del director de sprites (como el del hechizero o de la torre) para el enemigo

---
+++ src/core/sprite_manager.py
@@ -0,0 +1,13 @@
+import os
+import pygame
+
+class SpriteManager:
+    def __init__(self, base_path="assets/images"): #necesitamos una ruta base para que el sprite manager sepa cuales son los sprites a utilizar
+        self.base_path = base_path
+        self.sprites = {}
+
+    def load_sprite(self):
+        pass
+    
+    def get_sprite(self):
+        pass
