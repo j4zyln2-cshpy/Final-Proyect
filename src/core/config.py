d552c4de3bcb48a383ed70545d011826d64d7ddc
PetitCh <josepetitch@gmail.com>
2026-08-07 20:17:39 -0400

    Camvio de ubicación de archivo config.py, añadidura del directorio de a database

---
+++ src/core/config.py
@@ -0,0 +1,22 @@
+import os
+
+screen_width = 800 #ancho de pantalla
+screen_height = 600
+fps = 60
+game_title = "Kingdom Defene 1D"
+
+ground_y_position = 300
+tower_x_position = 700
+position_spawn_x = 0
+
+background_color = (40, 50, 60)
+text_color = (255, 255, 255)
+colorcito_orito = (255, 215, 0) #:3
+
+dir_base =  os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+dir_assets =  os.path.join(dir_base, "assets")
+dir_images =  os.path.join(dir_assets, "images")
+dir_sounds = os.path.join(dir_assets, "sounds")
+dir_database = os.path.join(dir_base, "database")
+
+#uploaded
