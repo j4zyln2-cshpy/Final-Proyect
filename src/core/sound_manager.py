2440896a08f7251ecf4b68dfa446ffff54498f6f
PetitCh <josepetitch@gmail.com>
2026-08-07 20:07:11 -0400

    Actualizando funciones para poder reproducir musica, y reproducir sonidos, todavía incompleto

--- src/core/sound_manager.py
+++ src/core/sound_manager.py
@@ -1,16 +1,33 @@
 import pygame
+import os
+from .config import dir_sounds
 
 class SoundManager:
     def __init__(self):
         pygame.mixer.init()
-        self.sound_click = pygame.mixer.Sound("assets/sounds/Confirm_Click.mp3")
-        self.sound_game_over = pygame.mixer.Sound("assets/sounds/Game_Over.mp3")
+        self.ruta_base = dir_sounds
+        self.sound_effects = {} #necesito identificar los efectos de clicks, como cofnrimaciones de clicks por ejemplo
+        #self.sound_click = pygame.mixer.Sound("assets/sounds/Confirm_Click.mp3") -- musica de clicks xd
+        #self.sound_game_over = pygame.mixer.Sound("assets/sounds/Game_Over.mp3") -- musica de game over
 
-    def play_music(self, file_path, loop = -1):
+    def play_music(self, file_name, loop = -1, volumencito = 0.4):
+      file_path = os.path.join(self.ruta_base, file_name)
+      try:
         pygame.mixer.music.stop()
-        pygame.mixer.music.load(file_path)
-        pygame.mixer.music.set_volume(0.4) #lo que sería volumen al 40%
-        pygame.mixer.music.play(loop)
+        pygame.mixer.music.load(file_path) #carga la musica del archivo
+        pygame.mixer.music.set_volume(volumencito) #lo que sería volumen al 40%
+        pygame.mixer.music.play(loop) #la reproduce en un ciclo determinado 
+      except Exception as e:
+         print(f"Problemita al reproducir música, {e}")
 
-    def play_sfx(self, sonido):
-        pass
+    def reproducir_sonidos_clicks (self, effect_s): #lo crré principalmente porque me gusta usar soniditos xd
+       if effect_s in self.sound_effects:
+          self.sound_effects[effect_s].play()
+       else:
+          print(f"Effect not founded <(＿　＿)>")
+       
+    def stop_all(self):
+       pygame.mixer.music.stop()
+       pygame.mixer.stop()
+
+    
