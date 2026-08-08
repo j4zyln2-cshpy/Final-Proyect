7136c15fe3566cd149e9aeb68fce0643ae3abd94
PetitCh <josepetitch@gmail.com>
2026-08-07 20:09:22 -0400

    Tuve que actualizar la lógica de guardar partida y director de guardado, necesarias para que el usuario pueda seguir con la partida si desea salir

--- states/save_manager.py
+++ states/save_manager.py
@@ -1,5 +1,34 @@
 import pygame
+import os
+import json
+from src.core.config import dir_database
 
 class SaveManager:
-    def __init__(self):
-        pass
+    def __init__(self, filename="savegame.json"):
+        self.filepath = os.path.join(dir_database, filename)
+
+    def guardar_partida(self, data_game):
+        try:
+           os.makedirs(os.path.dirname(self.filepath), exist_ok = True)
+          
+           with open(self.filepath, "w", encoding="utf-8") as pene:
+               json.dump(data_game, pene, indent = 4)
+           print(f"Partida guardada en {self.filepath}")
+           return True
+        except Exception as e:
+            print(f"Error al guardar partida: {e}")
+            return None
+
+    def cargar_partida(self):
+        if not os.path.exists(self.filepath):
+            print("No pudimos cargar el archivo papi :( ")
+            return None
+
+        try:
+            with open(self.filepath, "r", encoding="utf-8") as nose:
+                data = json.load(nose)
+            print("Partida cargada exitosamente")
+            return data
+        except Exception as e:
+            print(f"Error al cargar: {e}")
+            return None
--- states/save_state.py
+++ states/save_state.py
@@ -1,5 +1,40 @@
 import pygame
+from save_manager import SaveManager
+from base_state import BaseState
 
 class SaveState:
-    def __init__(self):
+    def __init__(self, game):
+        super().__init__(game)
+        self.font = self.font.SysFont("Arial" , 20)
+        self.save_manager = SaveManager()
+        self.mensaje = "Presiona 'G' para Guardar o 'ESC' para irte a donde quieras XD"
+
+    def handle_events(self, events):
+       for event in events:
+           if event.type == pygame.KEYDOWN:
+               if event.key == pygame.K_ESCAPE:
+                   self.game.change_state("GAME")
+
+               elif event.key == pygame.K_g:
+                   game_scene = self.game.estado_juego
+                   data = {
+                       "oro": game_scene.oro,
+                       "puntuacion": game_scene.puntuacion,
+                       "torre_vida": game_scene.torre.vida,
+                       "enemigos": [
+                           {"tipo": e.tipo, "x": e.x, "vida": e.vida} for e in game_scene.enemigos if e.activo
+                       ],
+                   }
+
+                   if self.save_manager.guardar_partida(data):
+                       self.mensaje = "Partida Guardada, Presiona ESC"
+                   else:
+                       self.mensaje = "Error al guardar. Intenta de nuevo"
+       
+    def update(self, dt):
         pass
+
+    def draw(self, surface):
+        surface.fill((20, 20, 30))
+        txt = self.font.render(self.mensaje, True, (255, 215, 0))
+        surface.blit(txt, (150, 190))
