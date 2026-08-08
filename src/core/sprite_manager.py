26b197151345ea6801d8fa1f8ddc209c65b16bc3
PetitCh <josepetitch@gmail.com>
2026-08-07 20:18:44 -0400

    Cambios en las funciones de obtener y cargar sprites, fue un dolor de cabeza para la prueba 4 xd

--- src/core/sprite_manager.py
+++ src/core/sprite_manager.py
@@ -6,8 +6,22 @@ class SpriteManager:
         self.base_path = base_path
         self.sprites = {}
 
-    def load_sprite(self):
-        pass
-    
-    def get_sprite(self):
-        pass
+    def load_sprite(self, name, filename):
+        path = os.path.join(self.base_path, filename)
+        try:
+           surface = pygame.image.load(path).convert_alpha()
+           self.sprites[name] = surface
+           print(f"Sprite cargado: {name}")
+        except pygame.error as e:
+           print(f"We have problems to change the sprite: {e}, sorry :( ")
+
+    def get_sprite(self, name, size=None):
+        if name not in self.sprites:
+            print(f"Warning: The sprite '{name}' it's not charged :v")
+            return None
+
+        sprite = self.sprites[name]
+        if size:
+            return pygame.transform.scale(sprite, size)
+        return sprite
+        
