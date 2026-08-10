f63ac242c3ea8992338c310e57675f428820567e
PetitCh <josepetitch@gmail.com>
2026-08-10 19:38:42 -0400

    Archivo principal, añadidura de funciones de guardado y obtener sprites

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 import sys
 
-#pd: todavía no voy a añadir ni el savemanager ni el save state, solo quiero confirmar algunas cosas antes de hacer colisiones y guardado
+from states.save_manager import SaveManager
+from states.save_state import SaveState
 from states.base_state import BaseState
 from states.game_over_state import GameOverState
 from states.pause_state import PauseState
@@ -10,6 +11,12 @@ from states.game_state import GameState
 from src.core.config import screen_width, screen_height, fps, game_title
 from src.core.sound_manager import SoundManager
 from src.core.sprite_manager import SpriteManager
+
+
+#este va a ser la fuente para la letra, de hecho, fue utilizada en la prueba xdxdd, a partir de mañana las voy a utilizar lol
+
+#font = pygame.font.Font('tests/pygame/font/Pixeltype.ttf', 50)
+#font_render= font.render('Kingdom Defense', 50, 'Black')
 
 class Game:
     def __init__(self):
@@ -31,15 +38,25 @@ class Game:
         }
         self.current_state = self.states["MENU"]
 
+
     def cargar_assets(self):
         sprites = {
             "mapa": "mapa_base.png",
-            "torre": "torre_rey.png",
+            "torre_rey": "torre_rey.png",
             "duende": "duende.png",
             "espadachin": "espadachin.png",
+            "caballero": "caballero.png" ,
+            "hechicero": "hechicero.png",
+            "soldado": "soldado.png",
+            "verdugo": "verdugo.png",
+            "ogro": "ogro.png",
+            "torre_base": "torre_base.png"
         }
         for key, file in sprites.items():
             self.sprite_manager.load_sprite(key, file)
+
+    def obtener_sprite(self, sprite):
+        return self.sprite_manager.get_sprite(sprite)
 
     def change_state(self, state_name):
         if state_name in self.states:
@@ -60,6 +77,17 @@ class Game:
             self.current_state.draw(self.screen)
 
             pygame.display.flip()
+
+    def save_file(self, data=None): #en realidad es solo para guardar tu registro actual, la data actual está vacía, necesitamos llenarla con la data del jugador
+        if data is None:
+            data = {
+                "jugador": {"gold": 150,"points": 300},
+                "torre_rey": {"hp": 100, "x_position": 650},
+                "partida": {"oleada_actual": 1,"Nivel": 1}
+            }
+        return self.save_manager.guardar_partida(data)
+
+# mañana habrán más funciones y más cambios
 
 if __name__ == "__main__":
     game = Game()
